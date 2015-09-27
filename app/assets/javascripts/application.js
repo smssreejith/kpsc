4a59eb24e601ebaba03eb0641170c4110a3eb551
Sreejith <smssreejith@gmail.com>
2015-09-27 16:03:09 +0530

    Basic layout with bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,11 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+$(document).ready(function() {
+$(".btn-pref .btn").click(function () {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below 
+    $(this).removeClass("btn-default").addClass("btn-primary");   
+});
+});
---
+++ app/controllers/exams_controller.rb
@@ -0,0 +1,10 @@
+class ExamsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end

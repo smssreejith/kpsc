710b205f7ce0bc4332f44b7ca4e94a4da9686b84
Sreejith <sreejith.gp@mobme.in>
2016-01-24 13:06:23 +0530

    unique index added to exams table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222060751) do
+ActiveRecord::Schema.define(version: 20160124072757) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "exam_id",    limit: 4
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20151222060751) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  add_index "exams", ["exam_type", "code"], name: "index_exams_on_exam_type_and_code", unique: true, using: :btree
 
   create_table "guest_users", force: :cascade do |t|
     t.datetime "created_at", null: false

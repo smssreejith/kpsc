e523a69968bf97d8b0bfe149e72b8ecfe7221ef3
Sreejith <sreejith.gp@mobme.in>
2016-02-20 09:37:47 +0530

    login as popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1512939478999206', 'e180cb3841ea7f7cf9bda77d679c5668', 
   :scope => 'email,user_location',
   :image_size => 'large',
-  :display => 'popup'
+  :authorize_params => { :display => 'popup'  }
 end
 

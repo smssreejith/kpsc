65c08ba429d4777256e6df3963b08eb817fbde9d
Sreejith <sreejith.gp@mobme.in>
2016-04-14 17:08:03 +0530

    fb login modified

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  #provider :facebook, '1512939478999206', 'e180cb3841ea7f7cf9bda77d679c5668', 
   provider :facebook, '1512939478999206', 'e180cb3841ea7f7cf9bda77d679c5668', 
   :scope => 'email,user_location',
   :image_size => 'large',
